9863905636b39afc9e5d4fb9ce6a0029968b19b5
valeomega <kruis14@hotmail.com>
2022-02-17 22:02:28 +0100

    Adjust names of auto setup with the new sql names

---
+++ out/build/x64-Debug/src/genrev/CMakeFiles/revision_data.h
@@ -0,0 +1,20 @@
+#ifndef __REVISION_DATA_H__
+#define __REVISION_DATA_H__
+ #define _HASH                      "unknown"
+ #define _DATE                      "1970-01-01 00:00:00 +0000"
+ #define _BRANCH                    "Archived"
+ #define _CMAKE_COMMAND             R"(C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe)"
+ #define _CMAKE_VERSION             R"(3.19.20122902-MSVC_2)"
+ #define _CMAKE_HOST_SYSTEM         R"(Windows 10.0.19043)"
+ #define _SOURCE_DIRECTORY          R"(C:/Users/Gebruiker/source/Repos/BfaCore-Reforged)"
+ #define _BUILD_DIRECTORY           R"(C:/Users/Gebruiker/source/Repos/BfaCore-Reforged/out/build/x64-Debug)"
+ #define _MYSQL_EXECUTABLE          R"(C:/Program Files/MySQL/MySQL Server 5.7/bin/mysql.exe)"
+ #define _FULL_DATABASE             "BFADB_837_world_2021_v0.1.sql"
+ #define _HOTFIXES_DATABASE         "BFADB_837_hotfixes_2021_v0.1.sql"
+ #define VER_COMPANYNAME_STR        "BfaCore Reforged"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2021 BfaCore Reforged"
+ #define VER_FILEVERSION            0,0,0
+ #define VER_FILEVERSION_STR        "unknown 1970-01-01 00:00:00 +0000 (Archived branch)"
+ #define VER_PRODUCTVERSION         VER_FILEVERSION
+ #define VER_PRODUCTVERSION_STR     VER_FILEVERSION_STR
+#endif // __REVISION_DATA_H__
