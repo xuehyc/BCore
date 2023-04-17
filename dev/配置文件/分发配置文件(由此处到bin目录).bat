729fa930d969b4d3f79d3fbcfce1a2c8638f03a0
小女孩 <xuehyc@163.com>
2023-04-17 20:04:07 +0800

    添加配置工具

---
+++ dev/配置文件/分发配置文件(由此处到bin目录).bat
@@ -0,0 +1,6 @@
+rem 注意此批处理,必须设置文本格式为ANSI,不然乱码,无法正常执行
+copy /Y "D:\Program Files (x86)\WOWDIY\BfaCore-Reforged\dev\配置文件\bnetserver.conf" "D:\Program Files (x86)\WOWDIY\BfaCore-Reforged\out\build\x64-Debug\bin"
+copy /Y "D:\Program Files (x86)\WOWDIY\BfaCore-Reforged\dev\配置文件\worldserver.conf" "D:\Program Files (x86)\WOWDIY\BfaCore-Reforged\out\build\x64-Debug\bin"
+
+copy /Y "D:\Program Files (x86)\WOWDIY\BfaCore-Reforged\dev\配置文件\bnetserver.conf" "D:\Program Files (x86)\WOWDIY\BfaCore-Reforged\out\build\x64-Release\bin"
+copy /Y "D:\Program Files (x86)\WOWDIY\BfaCore-Reforged\dev\配置文件\worldserver.conf" "D:\Program Files (x86)\WOWDIY\BfaCore-Reforged\out\build\x64-Release\bin"
